cea75999a16022123758e5b47977dc3396b856ed
ddnrt <58252564+ddnrt@users.noreply.github.com>
2020-06-30 06:14:17 +0300

    added switch overflow for body element ( off-canvas menu )

--- srcipt.js
+++ srcipt.js
@@ -1,9 +1,13 @@
 
 function openNav() {
   document.getElementById("mobile_nav").style.width = "100%";
+  var bodyElement = document.querySelector("body");
+  bodyElement.classList.add("overflow-active");
 
 }
 function closeNav() {
   document.getElementById("mobile_nav").style.width = "0";
+  var bodyElement = document.querySelector("body");
+  bodyElement.classList.remove("overflow-active");
 
 }
